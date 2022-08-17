c1286c574f6373a2a273e490a69c852b1d2f2ae2
Mateus Herculano <mateuherculano0@gmail.com>
2022-08-17 11:30:18 -0300

    refactor: styles global

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
+
 function App() {
-  return <h1>Welcome</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Welcome</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default App
