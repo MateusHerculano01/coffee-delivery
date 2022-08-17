1daa5cd57813020bb1222fd36259af1198ab6b45
Mateus Herculano <mateuherculano0@gmail.com>
2022-08-17 11:37:09 -0300

    feat: component home / routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { AppRoutes } from './Routes'
+
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Welcome</h1>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
