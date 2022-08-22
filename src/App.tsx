d74f85f71c6cff9a9fc39e4bb4a555a1a491cf9b
Mateus Herculano <mateuherculano0@gmail.com>
2022-08-22 16:57:15 -0300

    feat: context done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { CartContextProvider } from './contexts/CartContext'
 import { AppRoutes } from './Routes'
 
 import { GlobalStyle } from './styles/global'
@@ -8,9 +9,11 @@ import { defaultTheme } from './styles/theme/default'
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <CartContextProvider>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </CartContextProvider>
       <GlobalStyle />
     </ThemeProvider>
   )
