9b208c28b44aa9a8fa7958c043d6ca77ed6af96e
Mateus Herculano <mateuherculano0@gmail.com>
2022-08-22 21:25:37 -0300

    feat: all functions cart done

--- src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
+++ src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
@@ -1,28 +1,39 @@
+import { useCart } from '../../../../hooks/useCart'
 import { Button } from '../../../../components/Button'
 import { RegularText } from '../../../../components/Typography'
 import { ConfirmationSectionContainer } from './styles'
+import { formatMoney } from '../../../../utils/formatMoney'
+
+const DELIVERY_PRICE = 3.5
 
 export function ConfirmationSection() {
+  const { cartItemsTotal, cartQuantity } = useCart()
+  const cartTotal = DELIVERY_PRICE + cartItemsTotal
+
+  const formattedItemsTotal = formatMoney(cartItemsTotal)
+  const formattedCartTotal = formatMoney(cartTotal)
+  const formattedDeliveryPrice = formatMoney(DELIVERY_PRICE)
+
   return (
     <ConfirmationSectionContainer>
       <div>
         <RegularText size="s">Total de itens</RegularText>
-        <RegularText>R$ 9,90</RegularText>
+        <RegularText>R$ {formattedItemsTotal}</RegularText>
       </div>
       <div>
         <RegularText size="s">Entrega</RegularText>
-        <RegularText>R$ 3,50</RegularText>
+        <RegularText>R$ {formattedDeliveryPrice}</RegularText>
       </div>
       <div>
         <RegularText size="l" weight="700" color="subtitle">
           Total
         </RegularText>
         <RegularText size="l" weight="700" color="subtitle">
-          R$ 29,90
+          R$ {formattedCartTotal}
         </RegularText>
       </div>
 
-      <Button text="Confirmar Pedido" />
+      <Button text="Confirmar Pedido" disabled={cartQuantity <= 0} />
     </ConfirmationSectionContainer>
   )
 }
